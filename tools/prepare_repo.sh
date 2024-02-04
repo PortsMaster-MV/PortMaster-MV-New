aeaf4de9c9467177f30f33043797f096aad13f6f
Jacob <kloptops@gmail.com>
2024-02-04 12:31:17 +0800

    tools/prepare_repo.sh: fixed download links.

--- tools/prepare_repo.sh
+++ tools/prepare_repo.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+source SOURCE_SETUP.txt
+
 cd releases/
 rm -f *.json
 rm -f images.zip
-wget "https://github.com/PortsMaster/PortMaster-New/releases/latest/download/ports.json"
-wget "https://github.com/PortsMaster/PortMaster-New/releases/latest/download/ports_status.json"
-wget "https://github.com/PortsMaster/PortMaster-New/releases/latest/download/manifest.json"
-wget "https://github.com/PortsMaster/PortMaster-New/releases/latest/download/images.zip"
+wget "https://github.com/${RELEASE_ORG}/${RELEASE_REPO}/releases/latest/download/ports.json"
+wget "https://github.com/${RELEASE_ORG}/${RELEASE_REPO}/releases/latest/download/ports_status.json"
+wget "https://github.com/${RELEASE_ORG}/${RELEASE_REPO}/releases/latest/download/manifest.json"
+wget "https://github.com/${RELEASE_ORG}/${RELEASE_REPO}/releases/latest/download/images.zip"
 cd ..
 
 python3 tools/build_data.py
