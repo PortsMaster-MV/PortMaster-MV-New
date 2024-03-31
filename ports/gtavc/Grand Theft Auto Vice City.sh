df1ada06012a6a3ee8fc5b486bc4b6db82d5f853
Jacob <kloptops@gmail.com>
2024-03-31 14:52:32 +0800

    Fixed port headers to new standard header.

--- ports/gtavc/Grand Theft Auto Vice City.sh
+++ ports/gtavc/Grand Theft Auto Vice City.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# PORTMASTER: gtavc.zip, Grand Theft Auto Vice City.sh
 # Built from https://github.com/nosro1/re3 (branch miami-sdl2)
 
 PORTNAME="Grand Theft Auto Vice City"
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
   controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
 else
   controlfolder="/roms/ports/PortMaster"
 fi
@@ -45,3 +48,4 @@ $GPTOKEYB "reVC" &
 $ESUDO kill -9 $(pidof gptokeyb)
 $ESUDO systemctl restart oga_events &
 printf "\033c" >> $CUR_TTY
+
