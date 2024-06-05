7ae84de5b425c140a3d391ce0826ca8320f43f1b
JeodC <47716344+JeodC@users.noreply.github.com>
2024-06-05 08:24:01 -0400

    Initial 2ship port

---
+++ ports/soh2/Ship of Harkinian 2.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Set variables
+GAMEDIR="/$directory/ports/soh2"
+
+# Exports
+export LD_LIBRARY_PATH="$GAMEDIR/libs:/usr/lib"
+
+# Permissions
+$ESUDO chmod 0777 /dev/tty0
+
+cd $GAMEDIR
+
+# Remove old log to prevent bloat
+rm -rf "$GAMEDIR/logs/2 Ship 2 Harkinian.log"
+
+# Copy the right build to the main folder
+if [ $CFW_NAME == "ArkOS" ] || [ "$CFW_NAME" == 'ArkOS wuMMLe' ]; then
+	cp -f bin/compatibility.elf 2s2h.elf
+	if [ "$(find "./mods" -name '*.o2r')" ]; then
+		echo "WARNING: .OTR MODS FOUND! PERFORMANCE WILL BE LOW IF ENABLED!!" > /dev/tty0
+	fi
+else
+	cp -f bin/performance.elf 2s2h.elf
+fi
+
+# Run the game
+echo "Loading, please wait... (might take a while!)" > /dev/tty0
+$GPTOKEYB "2s2h.elf" -c "soh2.gptk" & 
+./2s2h.elf
+
+# Cleanup
+$ESUDO systemctl restart oga_events & 
+printf "\033c" >> /dev/tty1
+printf "\033c" > /dev/tty0
