5082e74f954eafc51f3d6f21e681c11869734468
JeodC <47716344+JeodC@users.noreply.github.com>
2024-10-21 20:23:23 -0400

    Add imgui.ini adjustments during launch

--- ports/soh/Ship of Harkinian.sh
+++ ports/soh/Ship of Harkinian.sh
@@ -13,9 +13,8 @@ else
 fi
 
 source $controlfolder/control.txt
-get_controls
-
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
 
 # Set variables
 GAMEDIR="/$directory/ports/soh"
@@ -32,11 +31,41 @@ cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 $ESUDO chmod +x -R $GAMEDIR/*
 
+# Check imgui.ini and modify if needed
+input_file="imgui.ini"
+temp_file="imgui_temp.ini"
+skip_section=0
+# Loop through each line in the input file
+while IFS= read -r line; do
+    # Check if the line is a window header
+    if [[ "$line" =~ ^\[Window\]\[Main\ Game\] || "$line" =~ ^\[Window\]\[Main\ -\ Deck\] ]]; then
+        skip_section=1  # Set the flag to skip modifications for this section
+    elif [[ "$line" =~ ^\[Window\] ]]; then
+        skip_section=0  # Reset the flag for other windows
+    fi
+
+    # Modify Pos and Size only if the current section is not skipped
+    if [[ $skip_section -eq 0 ]]; then
+        if [[ "$line" =~ ^Pos=.* ]]; then
+            echo "Pos=30,30" >> "$temp_file"
+        elif [[ "$line" =~ ^Size=.* ]]; then
+            echo "Size=400,300" >> "$temp_file"
+        else
+            echo "$line" >> "$temp_file"
+        fi
+    else
+        # If skipping, write the line unchanged
+        echo "$line" >> "$temp_file"
+    fi
+done < "$input_file"
+
+# Replace the original file with the modified one
+mv "$temp_file" "$input_file"
 
 # List of compatibility firmwares
-CFW_NAMES="ArkOS:ArkOS wuMMLe:ArkOS AeUX:knulli:TrimUI"
+CFW_NAMES="ArkOS:ArkOS wuMMLe:ArkOS AeUX:TrimUI"
 
-# Check if the current CFW name is in the list
+# Check if the current CFW name is in the compatibility list
 contains() {
     local value="$CFW_NAME"
     local item
@@ -66,6 +95,12 @@ else
     cp -f "$GAMEDIR/bin/performance.elf" "$GAMEDIR/soh.elf"
 fi
 
+# If we're using a TrimUI then use the compatibility binary
+# Knulli is compatible with TrimUI as well as higher GLIBC devices so we must also check the device name here
+if [[ "$DEVICE_NAME" == *"TrimUI"* ]]; then
+    cp -f "$GAMEDIR/bin/compatibility.elf" "$GAMEDIR/soh.elf"
+fi
+
 if [ ! -f "oot.otr" ] || [ ! -f "oot-mq.otr" ]; then
     # Ensure we have a rom file before attempting to generate otr
     if ls *.*64 1> /dev/null 2>&1; then
@@ -89,10 +124,9 @@ fi
 # Run the game
 echo "Loading, please wait... (might take a while!)" > $CUR_TTY
 $GPTOKEYB "soh.elf" -c "soh.gptk" & 
-pm_platform_helper soh.elf
+pm_platform_helper "soh.elf"
 ./soh.elf
 
 # Cleanup
 rm -rf "$GAMEDIR/logs/"
-
 pm_finish
