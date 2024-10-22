732eb3055f1a84b6f5b3dfdb1a0d45eaa58837ea
Jeod <47716344+JeodC@users.noreply.github.com>
2024-10-22 13:32:53 -0400

    Update Ship of Harkinian 2.sh
    Co-authored-by: Cyril Delétré <cyril.deletre@gmail.com>

--- ports/soh2/Ship of Harkinian 2.sh
+++ ports/soh2/Ship of Harkinian 2.sh
@@ -125,6 +125,4 @@ $GPTOKEYB "2s2h.elf" -c "soh2.gptk" &
 
 # Cleanup
 rm -rf "$GAMEDIR/logs/"
-$ESUDO systemctl restart oga_events & 
-printf "\033c" >> /dev/tty1
-printf "\033c" > /dev/tty0
+pm_finish
