d221261abe484b2efa7734e39e89b3b054167c69
JeodC <47716344+JeodC@users.noreply.github.com>
2024-11-03 18:17:55 -0500

    Update solarus ports

--- ports/tunics/Tunics!.sh
+++ ports/tunics/Tunics!.sh
@@ -22,7 +22,7 @@ solarus_dir="$HOME/portmaster-solarus"
 solarus_file="$controlfolder/libs/${runtime}.squashfs"
 
 # Exports
-export LD_LIBRARY_PATH="$GAMEDIR/libs:$solarus_dir"
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/libs:$solarus_dir"
 
 cd $GAMEDIR
 
@@ -53,7 +53,6 @@ $GPTOKEYB "$runtime" -c "tunics.gptk" &
 echo "Loading, please wait... (might take a while!)" > /dev/tty0
 "$runtime" $GAMEDIR/*.solarus 2>&1 | tee -a ./"log.txt"
 $ESUDO kill -9 $(pidof gptokeyb)
-$ESUDO umount "$solarus_file" || true
-$ESUDO systemctl restart oga_events & 
-printf "\033c" >> /dev/tty1
-printf "\033c" > /dev/tty0
+
+# Cleanup
+pm_finish
