37543986805c9866235230026350c7a106f91463
cdeletre <cyril.deletre@gmail.com>
2024-11-18 11:21:43 -0500

    Fix bad interpreter in install script and change the sequence.

--- ports/render96ex/render96ex.sh
+++ ports/render96ex/render96ex.sh
@@ -27,6 +27,8 @@ RES_DIR="res"
 BASEZIP="base.zip"
 DEMOS_DIR="demos"
 TEXTS_DIR="texts"
+VERSION="us"  # at the moment only US is supported and has been tested in Portmaster
+SM64US_MD5="20b854b239203baf6c961b850a4a51a2"
 
 mkdir -p "$CONFDIR"
 
@@ -39,29 +41,73 @@ export PATH="${GAMEDIR}/bin.${DEVICE_ARCH}:${PATH}"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 # Patcher config
-export PATCHER_FILE="$GAMEDIR/restool/install-res.sh"
+export PATCHER_FILE="$GAMEDIR/tools/install_mario64"
 export PATCHER_GAME="$(basename "${0%.*}")" # This gets the current script filename without the extension
 export PATCHER_TIME="about 10 minutes"
 
-# Check if patchlog.txt to skip patching
-if [ ! -f patchlog.txt ]; then
-    if [ -f "$controlfolder/utils/patcher.txt" ]; then
-        unzip "${RESTOOL_ZIP}"    
+# -------------------- BEGIN FUNCTIONS --------------------
 
-        if [ $? -eq 0 ];then
-          source "$controlfolder/utils/patcher.txt"
-        else
-          echo "Unpacking ${RESTOOL_ZIP} has failed"
-        fi
+check_mario()
+{
+  [[ ! -f $1 ]] && echo "error" && return 1
+  calc_md5=`md5sum "$1" | cut -d' ' -f1`
+  echo "calc_md5"
+  [[ "$calc_md5" == "$SM64US_MD5" ]]
+}
 
-        rm -rf ${RESTOOL_DIR}
-    else
-        echo "This port requires the latest version of PortMaster."
-        text_viewer -e -f 25 -w -t "PortMaster needs to be updated" -m "This port requires the latest version of PortMaster. Please update PortMaster first."
-        exit 0
+find_mario()
+{
+  echo "Searching rom file (expecting md5sum = $SM64US_MD5)"
+  find $1 -iname "*.z64" -print0 |
+  while IFS= read -r -d '' line; do
+    echo -n "Checking $line "
+    calc_md5=`check_mario "$line"`
+    if [[ $? -eq 0 ]];then
+      echo "- OK"
+      mv "${line}" "${GAMEDIR}/baserom.${VERSION}.z64"
+      return 0
     fi
-else
-    echo "Patching process already completed. Skipping."
+    echo "- WRONG md5sum ($calc_md5)"
+  done
+
+  [[ -f "${GAMEDIR}/baserom.${VERSION}.z64" ]] && return 0
+  echo "No rom found"
+  sleep 10
+  return 1
+}
+
+# --------------------- END FUNCTIONS ---------------------
+
+# Check if mandatory ressources are installed
+if [ ! -f $GAMEDIR/$RES_DIR/$BASEZIP ] || [ ! -d $GAMEDIR/$RES_DIR/$DEMOS_DIR ] || [ ! -d $GAMEDIR/$RES_DIR/$TEXTS_DIR ]
+then
+  echo "Ressources are missing."
+
+  echo "Looking for the rom"
+  # Check if a rom file is present
+  check_mario "${GAMEDIR}/baserom.${VERSION}.z64" || find_mario "${GAMEDIR}/"
+
+  if [ ! $? -eq 0 ]
+  then
+    echo "No baserom.${VERSION}.z64 file is present, can not proceed to the installation of the ressources"
+    text_viewer -e -f 25 -w -t "Error" -m "Oh, no! Ressources are missing. Install them first (put ${BASEROM} in ${GAMEDIR})."
+    exit 1
+  fi
+
+  echo "Okey dokey! The rom has been found. The installation of ressources will start"
+
+  if [ -f "$controlfolder/utils/patcher.txt" ]; then
+
+    source "$controlfolder/utils/patcher.txt"
+
+    rm -rf ${RESTOOL_DIR}
+
+  else
+      echo "This port requires the latest version of PortMaster."
+      text_viewer -e -f 25 -w -t "PortMaster needs to be updated" -m "This port requires the latest version of PortMaster. Please update PortMaster first."
+      exit 0
+  fi
+
 fi
 
 # Install a default sm64config.txt when it's missing
@@ -70,15 +116,9 @@ then
   cp sm64config.default.txt $CONFDIR/sm64config.txt
 fi
 
-# Check if mandatory ressources are installed before launching the game
-if [ ! -f $GAMEDIR/$RES_DIR/$BASEZIP ] || [ ! -d $GAMEDIR/$RES_DIR/$DEMOS_DIR ] || [ ! -d $GAMEDIR/$RES_DIR/$TEXTS_DIR ]
-then
-  echo "Ressources are missing."
-  text_viewer -e -f 25 -w -t "Error" -m "Oh, no! Ressources are missing. Install them first (put ${BASEROM} in ${GAMEDIR})."
-else
-  $GPTOKEYB "sm64.us.f3dex2e.${DEVICE_ARCH}" &
-  pm_platform_helper "$GAMEDIR/sm64.us.f3dex2e.${DEVICE_ARCH}"
-  ./sm64.us.f3dex2e.${DEVICE_ARCH} --savepath ./conf/
-fi
+$GPTOKEYB "sm64.us.f3dex2e.${DEVICE_ARCH}" &
+pm_platform_helper "$GAMEDIR/sm64.us.f3dex2e.${DEVICE_ARCH}"
+
+./sm64.us.f3dex2e.${DEVICE_ARCH} --savepath ./conf/
 
 pm_finish
