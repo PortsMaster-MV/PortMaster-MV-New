3d319d993914058487bfa5a63f00bf9984af6875
Jeod <47716344+JeodC@users.noreply.github.com>
2024-12-31 10:43:58 -0500

    Update AM2R.sh

--- ports/am2r/AM2R.sh
+++ ports/am2r/AM2R.sh
@@ -6,6 +6,8 @@ if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
   controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
 else
   controlfolder="/roms/ports/PortMaster"
 fi
