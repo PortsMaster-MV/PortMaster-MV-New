456f9abc57b936ac12fca32efff7ee90ef390af2
Jeod <47716344+JeodC@users.noreply.github.com>
2024-12-31 10:42:05 -0500

    Update AM2R.sh

--- ports/am2r/AM2R.sh
+++ ports/am2r/AM2R.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
-
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
   controlfolder="/opt/tools/PortMaster"
-elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
-  controlfolder="$XDG_DATA_HOME/PortMaster"
 else
   controlfolder="/roms/ports/PortMaster"
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
 export PORT_32BIT="Y"
-
-
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
 
-$ESUDO chmod 666 /dev/tty1
-$ESUDO chmod 666 /dev/uinput
-
+# Variables
 GAMEDIR="/$directory/ports/am2r"
 
-export LD_LIBRARY_PATH="$GAMEDIR/libs:/usr/lib:/usr/lib32:$LD_LIBRARY_PATH"
-$ESUDO rm -rf ~/.config/am2r
-ln -sfv $GAMEDIR/conf/am2r/ ~/.config/
+# CD and set permissions
 cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
+# Exports
+export LD_LIBRARY_PATH="$GAMEDIR/libs:/usr/lib:/usr/lib32:$LD_LIBRARY_PATH"
+
+# Config
+bind_directories "~/.config" "$GAMEDIR/conf/am2r"
+
+# Run game
 $GPTOKEYB "gmloader" -c "$GAMEDIR/am2r.gptk" &
-./gmloader gamedata/am2r.apk 2>&1 | tee $GAMEDIR/log.txt
-$ESUDO kill -9 $(pidof gptokeyb)
-unset LD_LIBRARY_PATH
-$ESUDO systemctl restart oga_events &
-printf "\033c" > /dev/tty1
+pm_platform_helper "$GAMEDIR/gmloader"
+./gmloader gamedata/am2r.apk
+
+# Cleanup
+pm_finish
 
