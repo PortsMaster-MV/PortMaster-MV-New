4b9a886d12afe97d4aee6477661d0bdac37f682c
Jeod <47716344+JeodC@users.noreply.github.com>
2025-02-26 13:37:05 -0500

    Add pokewilds

---
+++ ports/pokewilds/Pokewilds.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source "$controlfolder"/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/pokewilds"
+BINARY=java
+JAR_PACKAGE="${GAMEDIR}/bin/app/pokewilds.jar"
+
+# Logging
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Exports
+export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export XDG_DATA_HOME="$GAMEDIR/bin"
+export LANG=en_US.utf8
+export TEXTINPUTINTERACTIVE="Y"
+
+cd "$GAMEDIR"/bin || exit
+
+# Check for the jar
+if [[ ! -f $JAR_PACKAGE ]]; then
+    pm_message "Pokewilds jar package missing. Please follow the readme installation steps."
+    sleep 2
+    exit 1
+else
+    if [[ -d "$GAMEDIR/patch" ]]; then
+        # Patch in the modified classes to disable gamepad
+        "$GAMEDIR/tools/7za" u "${JAR_PACKAGE}" "$GAMEDIR"/patch/*
+        rm -rf "$GAMEDIR/patch"
+    fi
+fi
+
+# Display loading splash
+[ "$CFW_NAME" == "muOS" ] && $ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 1
+$ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 2000 & 
+
+# Check if we need to use westonpack. If we have mainline OpenGL, we don't need to use it.
+if glxinfo | grep -q "OpenGL version string"; then
+    westonpack=0
+else
+    westonpack=1
+fi
+
+if [ "$westonpack" -eq 1 ]; then
+    # Mount Weston
+    weston_dir=/tmp/weston
+    $ESUDO mkdir -p "${weston_dir}"
+    weston_runtime="weston_pkg_0.2"
+    if [ ! -f "$controlfolder/libs/${weston_runtime}.squashfs" ]; then
+        if [ ! -f "$controlfolder/harbourmaster" ]; then
+            pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
+            sleep 5
+            exit 1
+        fi
+        $ESUDO "$controlfolder"/harbourmaster --quiet --no-check runtime_check "${weston_runtime}.squashfs"
+    fi
+    if [[ "$PM_CAN_MOUNT" != "N" ]]; then
+        $ESUDO umount "${weston_dir}"
+    fi
+    $ESUDO mount "$controlfolder/libs/${weston_runtime}.squashfs" "${weston_dir}"
+fi
+
+# JRE load runtime
+runtime="zulu17.54.21-ca-jre17.0.13-linux"
+export JAVA_HOME="$HOME/zulu17.54.21-ca-jre17.0.13-linux.aarch64"
+$ESUDO mkdir -p "${JAVA_HOME}"
+
+if [ ! -f "$controlfolder/libs/${runtime}.squashfs" ]; then
+  # Check for runtime if not downloaded via PM
+  if [ ! -f "$controlfolder/harbourmaster" ]; then
+    pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
+    sleep 5
+    exit 1
+  fi
+
+  $ESUDO "$controlfolder"/harbourmaster --quiet --no-check runtime_check "${runtime}.squashfs"
+fi
+
+if [[ "$PM_CAN_MOUNT" != "N" ]]; then
+    $ESUDO umount "${JAVA_HOME}"
+fi
+
+$ESUDO mount "$controlfolder/libs/${runtime}.squashfs" "${JAVA_HOME}"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+# Run the game
+$GPTOKEYB "$BINARY" -c "$GAMEDIR/pokewilds.gptk" &
+pm_platform_helper "$JAVA_HOME/bin/java" >/dev/null
+if [ "$westonpack" -eq 1 ]; then 
+	$ESUDO env SDL_NO_SIGNAL_HANDLERS=1 WRAPPED_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH" WESTON_HEADLESS_WIDTH="$DISPLAY_WIDTH" WESTON_HEADLESS_HEIGHT="$DISPLAY_HEIGHT" \
+    "$weston_dir"/westonwrap.sh headless noop kiosk crusty_glx_gl4es \
+    PATH="$PATH" JAVA_HOME="$JAVA_HOME" XDG_DATA_HOME="$GAMEDIR/bin" java -Xmx612M -jar "${JAR_PACKAGE}"
+	$ESUDO "$weston_dir"/westonwrap.sh cleanup
+else
+	PATH="$PATH" JAVA_HOME="$JAVA_HOME" XDG_DATA_HOME="$GAMEDIR/bin" java -Xmx612M -Djava.awt.Window.fullscreen=true -jar "${JAR_PACKAGE}"
+fi
+
+# Cleanup
+pm_finish
