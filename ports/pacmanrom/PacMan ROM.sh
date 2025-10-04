59a14915e3e3be1a7771725861a0acabad807615
Slobters <132939843+slobters@users.noreply.github.com>
2025-10-04 23:19:52 +0200

    Added files for the new port PacMan ROM

---
+++ ports/pacmanrom/PacMan ROM.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# PORTMASTER: pacmanrom.zip, PacMan ROM.sh
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+export PORT_32BIT="Y"
+
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+GAMEDIR="/$directory/ports/pacmanrom"
+
+export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export GMLOADER_DEPTH_DISABLE=1
+export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
+export GMLOADER_PLATFORM="os_linux"
+
+# We log the execution of the script into log.txt
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+cd $GAMEDIR
+
+$GPTOKEYB "gmloader" -c ./pacmanrom.gptk &
+
+pm_platform_helper "$GAMEDIR/gmloader"
+
+./gmloader pacmanrom.apk
+
+pm_finish
---
+++ ports/pacmanrom/README.md
@@ -0,0 +1,27 @@
+## Notes
+
+Gameplay appears relatively consistent with the classic "PacMan". Primary differences are:
+
+* Player cannot see what PacMan cannot see
+* Ghosts behaviors are... unique.
+* Players who finish the original arcade game board suffer severe psychological and occasionally physiological trauma, up to and including death of the subject (ROM's effect is significantly reduced).
+
+## Acknowledgments
+
+A special thank you to [Berick Cook](https://berickcook.itch.io/pacman) for making this amazing cursed ghost game and allowing us to distribute it Ready to Run!
+
+Thanks to Fraxinus88 for making a [killer tutorial](https://github.com/Fraxinus88/GMloader-ports?tab=readme-ov-file) for porting GameMaker games;
+
+And finally a big thanks to our fellow testers and great port makers from the PortMaster Discord.
+
+
+## Controls
+
+| Button | Action |
+|--|--| 
+|D-pad / Left stick|Move|
+|A / B|Insert Coin|
+|X|Mute sound|
+|Y|Turn off CRT effect|
+
+
